47d40891a3c7376ec5ab66edc1c1f2f8cf090ac8
KimberllyMoreles <kim.moreles@hotmail.com>
2017-07-09 16:37:10 -0300

    Adicionar arquivos de projeto.

---
+++ SharePrice/SharePrice/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DryIoc;
+using Prism.DryIoc;
+using SharePrice.Views;
+using Xamarin.Forms;
+
+namespace SharePrice
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
