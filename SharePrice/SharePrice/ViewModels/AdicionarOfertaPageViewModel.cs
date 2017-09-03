b3f8953d10336f03ded0beeac0698c73ba502a35
KimberllyMoreles <kim.moreles@hotmail.com>
2017-09-03 14:23:30 -0300

    Tela de Usuários - Layout Adicionar Oferta

--- SharePrice/SharePrice/ViewModels/AdicionarOfertaPageViewModel.cs
+++ SharePrice/SharePrice/ViewModels/AdicionarOfertaPageViewModel.cs
@@ -1,16 +1,51 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace SharePrice.ViewModels
 {
     public class AdicionarOfertaPageViewModel : BaseViewModel
     {
-        public AdicionarOfertaPageViewModel()
-        {
+        private INavigationService _navigationService;
 
+        public Command AdicionarProdutoCommand { get; }
+        public Command AdicionarGeneroCommand { get; }
+        public Command LimparCommand { get; }
+        public Command SalvarCommand { get; }
+
+        public AdicionarOfertaPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            AdicionarProdutoCommand = new Command(async () => await ExecuteAdicionarProdutoCommandAsync());
+            AdicionarGeneroCommand = new Command(async () => await ExecuteAdicionarGeneroCommandAsync());
+            LimparCommand = new Command(async () => await ExecuteLimparCommandAsync());
+            SalvarCommand = new Command(async () => await ExecuteSalvarCommandAsync());
+        }
+
+        private Task ExecuteSalvarCommandAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task ExecuteLimparCommandAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task ExecuteAdicionarGeneroCommandAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task ExecuteAdicionarProdutoCommandAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
