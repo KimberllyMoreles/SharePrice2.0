f36d05500805d618a7d12c6458c496b207914085
KimberllyMoreles <kim.moreles@hotmail.com>
2017-10-25 23:30:02 -0200

    Protótipo da lista de ofertas

--- SharePrice/SharePrice/ViewModels/ListPageViewModel.cs
+++ SharePrice/SharePrice/ViewModels/ListPageViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using SharePrice.Models;
+using SharePrice.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,11 +15,33 @@ namespace SharePrice.ViewModels
     public class ListPageViewModel : BaseViewModel
     {
         private INavigationService _navigationService;
+        private OfertaService _ofertaService;
+        public ObservableCollection<Oferta> Ofertas { get; set; }
+        public DelegateCommand RefreshCommand { get; set; }
+
 
         public ListPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            _ofertaService = new OfertaService();
 
+            Ofertas = new ObservableCollection<Oferta>();
+            RefreshCommand = new DelegateCommand(CarregarOfertasAsync);
+
+            CarregarOfertasAsync();
+        }
+
+        public async void CarregarOfertasAsync()
+        {
+            var result = await _ofertaService.GetOfertas();
+
+            Ofertas.Clear();
+
+            foreach (var item in result)
+            {
+                Ofertas.Add(item);
+            }
+            IsBusy = false;
         }
     }
 }
--- SharePrice/SharePrice/Views/ListPage.xaml.cs
+++ SharePrice/SharePrice/Views/ListPage.xaml.cs
@@ -9,17 +9,6 @@ namespace SharePrice.Views
         public ListPage()
         {
             InitializeComponent();
-            list.ItemsSource = new List<Oferta>
-{
-    new Oferta { ProdutoId = "Angelina Figueira", Preco = 15 },
-    new Oferta { ProdutoId = "Angelino Neiva", Preco = 20 },
-    new Oferta { ProdutoId = "Boaventura Pardo", Preco = 25 },
-    new Oferta { ProdutoId = "Cristóvão Peseiro", Preco = 28 },
-    new Oferta { ProdutoId = "Osvaldo Costa", Preco = 31 },
-    new Oferta { ProdutoId = "Priscila Bento", Preco = 47 },
-    new Oferta { ProdutoId = "Sandoval Rocha", Preco = 19 },
-    new Oferta { ProdutoId = "Veríssimo Porciúncula", Preco = 34 }
-};
         }
     }
 }
